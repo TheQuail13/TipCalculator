2942d41fb5ffe2d208552f48cda3146e3cb71205
TheQuail13 <dougthierjung@gmail.com>
2018-07-21 11:18:57 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+      :clipped-left="clipped"
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>web</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="fixed = !fixed">
+        <v-icon>remove</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: true,
+      drawer: false,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <form>
+          <v-text-field v-model="tipTotal" label="Tips" required></v-text-field>
+        </form>
+        <v-flex>
+          <user v-for="user in users" :key="user.id" v-bind:user="user"></user>
+        </v-flex>
+      </v-layout>
+    </v-slide-y-transition>
+    <v-btn absolute dark fab bottom right color="blue" @click="addUser">
+      <v-icon>add</v-icon>
+    </v-btn>
+  </v-container>
+</template>
+
+<script>
+import User from '@/components/User'
+
+export default {
+  components: {
+    User
+  },
+  data () {
+    return {
+      tipTotal: 0,
+      hoursTotal: 0,
+      users: [
+        {
+          id: 1,
+          name: 'Towelie',
+          hours: 6,
+          tips: 0
+        },
+        {
+          id: 2,
+          name: 'Washcloth',
+          hours: 8,
+          tips: 0
+        }
+      ]
+    }
+  },
+  methods: {
+    getUsers: function () {
+      console.log('Return user info here')
+    },    
+    addUser: function () {
+      
+    }
+  },
+  created() {
+    this.getUsers()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

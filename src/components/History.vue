ca76e0900b3655b39b31a89821dff46fc8f2dc48
TheQuail13 <dougthierjung@gmail.com>
2018-08-09 18:38:21 -0700

    Added to history tab - total hours and tips

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,8 +1,19 @@
 <template>
      <v-container fluid grid-list-md text-xs-center>
          <v-layout column>
-             <v-flex xs12 v-for="(days, index) in completedDays" :key="index">
-                  <div>{{convertTimestampToDate(days.date.seconds)}}</div>
+             <v-flex xs12 v-for="(day, index) in completedDays" :key="index">
+                 <v-card>
+                     <v-card-title primary-title>
+                         <div>
+                            <div class="headline">{{convertTimestampToDate(day.date.seconds)}}</div>
+                            <div class="grey--text">Total Hours: {{sumHours(day.employees)}}</div>
+                            <div class="grey--text">Total Tips: {{formatAsCurrency(sumTips(day.employees))}}</div>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn flat color="blue">View Details</v-btn>
+                    </v-card-actions>
+                 </v-card>
              </v-flex>
          </v-layout>
      </v-container>
@@ -28,8 +39,23 @@ export default {
         convertTimestampToDate: function (timeStampSeconds) {
             var newDate = new Date(0)
             newDate.setUTCSeconds(timeStampSeconds);
-            return newDate
-        }
+            return newDate.toLocaleDateString("en-US")
+        },
+        sumHours: function (day) {
+            return day.reduce((total, emp) => total + emp.hours, 0)
+        },
+        sumTips: function (day) {
+            return day.reduce((total, emp) => total + emp.tips, 0)
+        },
+        formatAsCurrency (value) {
+            var formatter = new Intl.NumberFormat('en-us', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 2
+            })
+
+            return formatter.format(value)
+        },
     },
     computed: {
         completedDays: {
